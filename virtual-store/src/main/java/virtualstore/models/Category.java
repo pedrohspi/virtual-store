b4d764afb94cb46be90945fd68ee5c6532a5044f
Pedro Henrique Sarmento Pereira <pedro.pereira@ccc.ufcg.edu.br>
2025-01-28 11:01:01 -0300

    feat: create Category class for Product categorization

---
+++ virtual-store/src/main/java/virtualstore/models/Category.java
@@ -0,0 +1,59 @@
+package virtualstore.models;
+
+import java.io.Serializable;
+import java.util.Objects;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "category")
+@SequenceGenerator(name = "category_sequence", sequenceName = "category_sequence", allocationSize = 1, initialValue = 1)
+public class Category implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "category_sequence")
+	private Long id;
+	
+	@Column(nullable = false)
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return Objects.equals(id, other.id);
+	}
+}
