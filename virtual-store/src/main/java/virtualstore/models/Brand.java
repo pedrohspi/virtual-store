1a065b64d005e20e5ff35479947391ad7a4a9ca8
Pedro Henrique Sarmento Pereira <pedro.pereira@ccc.ufcg.edu.br>
2025-01-28 11:13:59 -0300

    fix: correction of attributes in Brand class

--- virtual-store/src/main/java/virtualstore/models/Brand.java
+++ virtual-store/src/main/java/virtualstore/models/Brand.java
@@ -1,5 +1,8 @@
 package virtualstore.models;
+
 import java.io.Serializable;
+import java.util.Objects;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,13 +12,48 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "brand")
-@SequenceGenerator(name = "seq_product_brand", sequenceName = "seq_product_brand", allocationSize = 1, initialValue = 1)
+@SequenceGenerator(name = "brand_sequence", sequenceName = "brand_sequence", allocationSize = 1, initialValue = 1)
 public class Brand implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_product_brand")
+	
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "brand_sequence")
 	@Id
 	private Long id;
 	
+	@Column(nullable = false)
 	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Brand other = (Brand) obj;
+		return Objects.equals(id, other.id);
+	}	
 }
